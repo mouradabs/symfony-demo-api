47c4677c4432d6792be30c275a57e8bbb1eec37c
Elie CHARRA <elie.charra@gmail.com>
2016-10-22 20:48:22 +0200

    Try doctrine partial select

--- src/AppGraphQLBundle/Resolver/DoctrineResolver.php
+++ src/AppGraphQLBundle/Resolver/DoctrineResolver.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace AppGraphQLBundle\Resolver;
-use AppBundle\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Parser\Ast\Field;
+use Youshido\GraphQL\Parser\Ast\Query;
 use Youshido\GraphQL\Type\Scalar\AbstractScalarType;
 use Youshido\GraphQL\Type\Scalar\IntType;
 
@@ -54,7 +54,7 @@ abstract class DoctrineResolver
         }
     }
 
-    protected function addJoinTypes(QueryBuilder $qb, ResolveInfo $info)
+    protected function iterateOnFields(ResolveInfo $info, \Closure $callback)
     {
         $type = $info->getField()
             ->getType()
@@ -69,15 +69,45 @@ abstract class DoctrineResolver
         $fieldList = $info->getFieldASTList();
 
         /** @var Field $field */
-        $joinIndex = 0;
-        foreach ($fieldList as $field) {
+        foreach ($fieldList as $index => $field) {
             $fieldType = $fieldTypeList[$field->getName()]->getType();
-            if (!$fieldType instanceof AbstractScalarType) {
-                $qb->leftJoin(self::ALIAS . '.' . $field->getName(), 'r_' . $joinIndex)
-                    ->addSelect('r_' . $joinIndex);
-                $joinIndex++;
-            }
+            $callback($field, $fieldType, $info, $index);
         }
+    }
+
+    protected function addJoinTypes(QueryBuilder $qb, ResolveInfo $info)
+    {
+        $this->iterateOnFields($info, function($field, $fieldType, ResolveInfo $info, $index) use ($qb) {
+            if (!$fieldType instanceof AbstractScalarType) {
+                $fieldList = $info->getFieldASTList();
+                $selectSubFields = ['id'];
+                foreach ($fieldList as $mField) {
+                    if ($mField->getName() === $field->getName() &&
+                        $mField instanceof Query) {
+                        foreach ($mField->getFields() as $subField) {
+                            $selectSubFields[] = $subField->getName();
+                        }
+                    }
+                }
+                $qb->leftJoin(self::ALIAS . '.' . $field->getName(), 'r_' . $index)
+                    ->addSelect('partial r_' . $index . sprintf('.{%s}', implode($selectSubFields, ',')));
+            }
+        });
+    }
+
+    protected function getSelectFields(ResolveInfo $info) : array
+    {
+        $selectList = [
+            'id'
+        ];
+
+        $this->iterateOnFields($info, function($field, $fieldType) use (&$selectList) {
+            if ($fieldType instanceof AbstractScalarType) {
+                $selectList[] = $field->getName();
+            }
+        });
+
+        return $selectList;
     }
 
     public function resolveTotal() : int
@@ -96,8 +126,11 @@ abstract class DoctrineResolver
     {
         $qb = $this->entityManager->createQueryBuilder();
 
-        $qb->select(self::ALIAS)
-            ->from($this->getEntity(), self::ALIAS);
+        $selectFieldList = $this->getSelectFields($info);
+        $selectFieldList = sprintf('partial e.{%s}', implode($selectFieldList, ','));
+
+        $qb->select($selectFieldList)
+            ->from($this->getEntity(), 'e');
 
         $this->addJoinTypes($qb, $info);
 
